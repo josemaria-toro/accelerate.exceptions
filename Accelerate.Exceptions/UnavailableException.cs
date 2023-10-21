f906341fe8686c602edd19edce7febc2d494ed1e
José Mª Toro Bermejo <118587313+josemaria-toro@users.noreply.github.com>
2023-10-21 05:56:42 +0000

    - Included exception for errors related to business processes or rules. - Included exception for errors related to duplicate values or objects. - Included exception for errors when a value or objects is not found. - Included exception for errors related to security. - Included exception for errors related to availability of systems. - Included exception for errors related to validations.

---
+++ Accelerate.Exceptions/UnavailableException.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Serialization;
+
+namespace Accelerate
+{
+    /// <summary>
+    /// Represent an error that occur when a remote endpoint is unavailable.
+    /// </summary>
+    [Serializable]
+    public class UnavailableException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnavailableException" /> class.
+        /// </summary>
+        public UnavailableException() : base()
+        {
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnavailableException" /> class with a specified error message.
+        /// </summary>
+        /// <param name="message">
+        /// The message that describes the error.
+        /// </param>
+        public UnavailableException(String message) : base(message)
+        {
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnavailableException" /> class with a specified error message and a reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="message">
+        /// The message that describes the error.
+        /// </param>
+        /// <param name="innerException">
+        /// The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner exception is specified.
+        /// </param>
+        public UnavailableException(String message, Exception innerException) : base(message, innerException)
+        {
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnavailableException" /> class with serialized data.
+        /// </summary>
+        /// <param name="info">
+        /// The <see cref="SerializationInfo" /> that holds the serialized object data about the exception being thrown.
+        /// </param>
+        /// <param name="context">
+        /// The <see cref="StreamingContext" /> that contains contextual information about the source or destination.
+        /// </param>
+        [ExcludeFromCodeCoverage]
+        [Obsolete("This constructor is obsolete for security reasons")]
+        protected UnavailableException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
